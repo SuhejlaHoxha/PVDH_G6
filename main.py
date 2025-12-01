1cf4e9144793f5e244599ef4bdf75f3623532977
Suhejla Hoxha <121345932+SuhejlaHoxha@users.noreply.github.com>
2025-12-01 13:30:50 +0100

    Implement outlier detection in main.py

--- main.py
+++ main.py
@@ -56,5 +56,9 @@ def main():
     tfm_info = run_transformations(bin_out, tfm_out)
     print("Transformations summary:", tfm_info)
 
+    print("\n--- OUTLIER DETECTION ---\n")
+    outlier_info = detect_outliers(CSV_INPUT)
+    print(outlier_info)
+
 if __name__ == "__main__":
     main()
